9e121f9de4fc0732be764d95e12f91b2f6ccefeb
okm <kdk_okm14@mirror.ocn.ne.jp>
2015-09-27 01:35:32 +0900

    format
    use 2 spaces

--- 01.sbt/src/main/scala/FizBuz.scala
+++ 01.sbt/src/main/scala/FizBuz.scala
@@ -1,8 +1,8 @@
 object FizBuz {
-	def apply(x: Int):String = {
-		var ret:String = ""
-		if (x % 3 == 0) ret += "fiz"
-		if (x % 5 == 0) ret += "buz"
-		ret
-	}
+  def apply(x: Int): String = {
+    var ret:String = ""
+    if (x % 3 == 0) ret += "fiz"
+    if (x % 5 == 0) ret += "buz"
+    ret
+  }
 }

a7b4ca5e45faf179b2c2241c4695affcf9c5a543
okm <kdk_okm14@mirror.ocn.ne.jp>
2015-09-27 01:40:13 +0900

    refactor
    use pattern match

--- 01.sbt/src/main/scala/FizBuz.scala
+++ 01.sbt/src/main/scala/FizBuz.scala
@@ -1,8 +1,8 @@
 object FizBuz {
-  def apply(x: Int): String = {
-    var ret:String = ""
-    if (x % 3 == 0) ret += "fiz"
-    if (x % 5 == 0) ret += "buz"
-    ret
+  def apply(x: Int) = x match {
+    case x if x % 15 == 0 => "fizbuz"
+    case x if x % 3  == 0 => "fiz"
+    case x if x % 5  == 0 => "buz"
+    case _ => ""
   }
 }
